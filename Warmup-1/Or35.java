ef96f1df4adccbd62913e52a40454c3859d9d226
Nitish k <91598274+Nitish36@users.noreply.github.com>
2022-05-14 11:05:58 +0530

    Add files via upload

---
+++ Warmup-1/Or35.java
@@ -0,0 +1,20 @@
+package codingbat;
+
+public class Or35 {
+	public boolean or35(int n) {
+		  if(n%3 == 0 || n%5==0)
+		  {
+		    return true;
+		  }
+		  else
+		  {
+		    return false;
+		  }
+		}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
