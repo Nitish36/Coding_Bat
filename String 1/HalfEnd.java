215f7daf016ed5ffe6cf6f9640a2b5e8fd1da706
Nitish k <91598274+Nitish36@users.noreply.github.com>
2022-05-16 10:08:44 +0530

    Add files via upload

---
+++ String 1/HalfEnd.java
@@ -0,0 +1,14 @@
+package codingbat;
+
+public class HalfEnd {
+	public String withoutEnd(String str) {
+		  String half = str.substring(1,str.length()-1);
+		  return half;
+		}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
