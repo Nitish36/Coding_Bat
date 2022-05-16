49ff90393757ac57aeaccec26efd69b30dfb8b5f
Nitish k <91598274+Nitish36@users.noreply.github.com>
2022-05-16 09:38:11 +0530

    Add files via upload

---
+++ String 1/Stringabba.java
@@ -0,0 +1,13 @@
+package codingbat;
+
+public class Stringabba {
+	public String makeAbba(String a, String b) {
+		  return a+b+b+a;
+		}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
