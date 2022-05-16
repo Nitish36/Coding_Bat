30dd7fce25adb51905192a94727878276e574059
Nitish k <91598274+Nitish36@users.noreply.github.com>
2022-05-16 10:18:24 +0530

    Add files via upload

---
+++ String 1/NonStart.java
@@ -0,0 +1,13 @@
+package codingbat;
+
+public class NonStart {
+	public String nonStart(String a, String b) {
+		  return a.substring(1,a.length())+b.substring(1,b.length());
+		}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
