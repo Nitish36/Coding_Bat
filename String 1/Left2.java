b349b4127854009e4a56900bfb779b3b1a07e700
Nitish k <91598274+Nitish36@users.noreply.github.com>
2022-05-16 10:21:52 +0530

    Add files via upload

---
+++ String 1/Left2.java
@@ -0,0 +1,13 @@
+package codingbat;
+
+public class Left2 {
+	public String left2(String str) {
+		  return str.substring(2,str.length())+str.substring(0,2);
+		}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
