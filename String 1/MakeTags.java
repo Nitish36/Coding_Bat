8868d596a16c839125226555b5055c6f97a251a9
Nitish k <91598274+Nitish36@users.noreply.github.com>
2022-05-16 09:44:31 +0530

    Add files via upload

---
+++ String 1/MakeTags.java
@@ -0,0 +1,13 @@
+package codingbat;
+
+public class MakeTags {
+	public String makeTags(String tag, String word) {
+		  return "<"+tag+">"+word+"</"+tag+">";
+		}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
