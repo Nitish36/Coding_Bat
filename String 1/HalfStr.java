4e9862a8b166adf753cca8e2d3107c56fb6ebc5c
Nitish k <91598274+Nitish36@users.noreply.github.com>
2022-05-16 10:05:01 +0530

    Add files via upload

---
+++ String 1/HalfStr.java
@@ -0,0 +1,15 @@
+package codingbat;
+
+public class HalfStr {
+	public String firstHalf(String str) {
+		  int n = str.length()/2;
+		  String halfStr = str.substring(0,n);
+		  return halfStr;
+		}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
