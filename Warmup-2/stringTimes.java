4dc15c4d33e720f2548d9444db7c156050bfb4f8
Nitish k <1rn19cs092.nitish.pkv@gmail.com>
2023-03-13 15:37:26 +0530

    Create stringTimes.java

---
+++ Warmup-2/stringTimes.java
@@ -0,0 +1,9 @@
+public String stringTimes(String str, int n) {
+  StringBuilder sb = new StringBuilder();
+  for(int i=0;i<n;i++)
+  {
+    sb.append(str);
+  }
+  String s = sb.toString();
+  return s;
+}
